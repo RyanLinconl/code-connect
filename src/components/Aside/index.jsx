91b7572516973c934c8c33191b089ae36f722253
RyanLinconl <ryanlinconl1234@gmail.com>
2025-04-10 14:59:15 -0300

    v2.0

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -2,11 +2,14 @@ import Image from 'next/image'
 import Styles from './aside.module.css'
 
 import Logo from './logo.png'
+import Link from 'next/link'
 
 export const Aside = () => {
     return (
         <aside className={Styles.aside}>
+            <Link href={'/'}>
             <Image src={Logo} alt='Logo da Code Connect' />
+            </Link>
         </aside>
     )
 }
