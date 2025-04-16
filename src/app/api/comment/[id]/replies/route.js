71499d3710d212c8efabdbd384fe37ebd6e0f85d
RyanLinconl <ryanlinconl1234@gmail.com>
2025-04-16 09:32:46 -0300

    v4.9

--- src/app/api/comment/[id]/replies/route.js
+++ src/app/api/comment/[id]/replies/route.js
@@ -1,13 +1,20 @@
 import db from "../../../../../../prisma/db";
 
 export async function GET(_request, { params }) {
+    const id = parseInt(params.id);
+
+    if (isNaN(id)) {
+        return new Response(JSON.stringify({ error: 'Invalid ID' }), { status: 400 });
+    }
+
     const replies = await db.comment.findMany({
         where: {
-            parentId: parseInt(params.id)
+            parentId: id
         },
         include: {
             author: true
         }
-    })
-    return Response.json(replies)
+    });
+
+    return Response.json(replies);
 }
